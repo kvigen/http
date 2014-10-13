4b1601549f830c13406e50e361e31250c5770a93
kvigen <kvigen@gmail.com>
2014-10-12 17:16:35 -0700

    Create process per connection
    This code allows the HTTP server to handle multiple connections simultanteously.
    It does this by creating a process for each connection. This clearly isn't the
    ideal solution as creating a process is pretty expensive, but it's a good simple
    solution that provides connection isolation.
    
    In the future I may implement threaded connections, or possibly even some kind
    of event based system.

--- server.c
+++ server.c
@@ -1,19 +1,26 @@
 #include <fcntl.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/sendfile.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include "utils.c"
 
 void doWork(int socketFd);
+void handleHttpRequest(int socketFd);
+int readHttpRequest(int socketFd);
+void sigchldHandler(int sig); 
+
 
 int main(int argc, char* argv[]) {
 
+  // TODO: This should become a daemon process and handle SIGHUP
   struct addrinfo hints;
   memset(&hints, 0, sizeof(struct addrinfo));
   hints.ai_canonname = NULL;
@@ -31,6 +38,14 @@ int main(int argc, char* argv[]) {
     exit(1); // TODO: Use the constant here...
   }
 
+  struct sigaction sa;
+  sa.sa_flags = SA_RESTART; // This is important because it restarts system calls on failure
+  sa.sa_handler = sigchldHandler;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("Error establishing the SIGCHLD handler");
+    exit(EXIT_FAILURE);
+  }
+
   // Just use the first addrInfo we have - in theory we would have to loop
   // through, but I can't remember why...
   int socketFd = socket(addrInfo->ai_family, addrInfo->ai_socktype, addrInfo->ai_protocol);
@@ -38,21 +53,30 @@ int main(int argc, char* argv[]) {
   int socketOption = 1;
   if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &socketOption, sizeof(socketOption)) == -1) {
     perror("Error setting socket option");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
   // TODO: Set more socket options...
   if (bind(socketFd, addrInfo->ai_addr, addrInfo->ai_addrlen) != 0) {
     perror("Couldn't bind socket");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
 
-  // TODO: Play around with the backlog argument. It's the 2nd one
-  if (listen(socketFd, 0) == -1) {
+  if (listen(socketFd, SOMAXCONN) == -1) {
     perror("Couldn't listen on socket");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
   doWork(socketFd);
   return 0;
+}
+
+void sigchldHandler(int sig) {
+  // Get all the children we can, without waiting
+  int status;
+  while (waitpid(-1, &status, WNOHANG) > 0) {
+    if (status != 0) {
+      printf("Unexpected child exit code: %d", status);
+    }
+  }
 }
 
 void write404(int clientFd);
@@ -63,24 +87,42 @@ void doWork(int socketFd) {
   socklen_t addrLength = sizeof(struct sockaddr_storage);
   for (;;) {
     // TODO: Understand why we cast here...
-    int clientFd = accept(socketFd, (struct sockaddr*)&addrStorage, &addrLength);
+    //int clientFd = accept(socketFd, (struct sockaddr*)&addrStorage, &addrLength);
+    int clientFd = accept(socketFd, NULL, NULL);
     if (clientFd == -1) {
       perror("Failed to accept socket");
       continue;
     }
-    // Could get information about the connecting socket, but for now I'll ignore that...
-    int forIndex = readHttpRequest(clientFd);
-    // TODO: It would be cool to support re-direction
-    if (forIndex) {
-      write200(clientFd);
-    } else {
-      write404(clientFd);
+    switch (fork()) {
+    case -1:
+      perror("Couldn't create child process");
+      exit(EXIT_FAILURE);
+
+    case 0:
+      // The child should close the listening socket because it doesn't need it
+      close(socketFd);
+      handleHttpRequest(clientFd);
+      _exit(EXIT_SUCCESS);
+
+    default:
+      close(clientFd);
+      // The parent then continues about their work
     }
-    fprintf(stderr, "Got what we expected: %d", forIndex);
-    // Write something back to the client...
-    if (close(clientFd) == -1) {
-      perror("Error closing");
-    }
+  }  
+}
+
+void handleHttpRequest(int clientFd) {
+  // Could get information about the connecting socket, but for now I'll ignore that...
+  int forIndex = readHttpRequest(clientFd);
+  sleep(3);
+  // TODO: It would be cool to support re-direction
+  if (forIndex) {
+    write200(clientFd);
+  } else {
+    write404(clientFd);
+  }
+  if (close(clientFd) == -1) {
+    perror("Error closing");
   }
 }
 
@@ -95,7 +137,6 @@ int readHttpRequest(int clientFd) {
     return 0;
   }
   // TODO: Be a bit smarter about this. We should be able to just split on the space.
-  fprintf(stderr, buffer);
   if (strcmp("GET /index.html HTTP/1.1", buffer) == 0) {
     returnVal = 1; 
   }
